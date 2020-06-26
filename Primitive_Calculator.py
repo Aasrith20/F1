c7174279e19d7bc7dbdf2510e7d75072b5fb4370
Aasrith20 <62925727+Aasrith20@users.noreply.github.com>
2020-06-26 19:42:17 +0530

    Update Primitive_Calculator.py

--- Primitive_Calculator.py
+++ Primitive_Calculator.py
@@ -1,47 +1,52 @@
-#Does not pass for 100000
 n=int(input())
-a=[0,0]
-b=[0,0]
+source=[0,0]
+trace=[0,0]
 for i in range(2,n+1):
-    w=int(i/3)
-    e=int(i/2)
+    a,b,c=0,0,0
+    w=i//3
+    e=i//2
     #print(i,w,e)
     if i%2==0 and i%3==0:
-        t=min(a[e],a[w],a[i-1])
+        t=min(source[e],source[w],source[i-1])
         q=t+1
-        a.append(q)
+        source.append(q)
+        a,b=1,1
         #print("first")
     elif i%2==0 and i%3!=0:
-        t=min(a[e],a[i-1])
+        t=min(source[e],source[i-1])
         q=t+1
-        a.append(q)
+        a=1
+        source.append(q)
         #print("second")
     elif i%3==0 and i%2!=0:
-        t=min(a[w],a[i-1])
+        t=min(source[w],source[i-1])
         q=t+1
-        a.append(q)
+        b=1
+        source.append(q)
         #print("third")
     else:
-        t=a[i-1]
-        a.append(a[i-1]+1)
+        t=source[i-1]
+        source.append(source[i-1]+1)
         #print("fourth")
-    if t==a[i-1]:
+    if t==source[i-1] and i%1==0:
         #print(i,i-1,"third",sep="|")
-        b.append(i-1)
-    elif t==a[w]:
+        trace.append(i-1)
+    elif t==source[w] and b==1 :
         #print(i,w,"second",sep="|")
-        b.append(w)
+        trace.append(w)
     else:
         #print(i,e,"first",sep="|")
-        b.append(e)
-#print(list(enumerate(b)))
-#print(list(enumerate(a)))
-#print(a[n])
+        trace.append(e)
+#print(list(enumerate(trace)))
+#print(list(enumerate(source)))
+print(source[n])
 element=0
 ans=[n]
 if n>1:
     while(element!=1):
-        element=b[n]
+        element=trace[n]
         ans.append(element)
         n=element
     print(" ".join(str(i) for i in reversed(ans)))
+else:
+    print(1)
